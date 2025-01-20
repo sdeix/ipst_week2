aac705e004f7ffbed06e022f6668c54b4b458292
sdeix <you@example.com>
2025-01-20 18:47:28 +0700

    TS types have been generated based on SQL tables with codegen.

--- app/common/types/kysely/db.type.ts
+++ app/common/types/kysely/db.type.ts
@@ -9,13 +9,35 @@ export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
   ? ColumnType<S, I | undefined, U>
   : ColumnType<T, T | undefined, T>;
 
+export type Timestamp = ColumnType<Date, Date | string, Date | string>;
+
+export interface Objectives {
+  createdAt: Timestamp;
+  creatorid: string;
+  description: string | null;
+  id: Generated<string>;
+  isCompleted: boolean | null;
+  notifyAt: Timestamp | null;
+  title: string;
+  updatedAt: Timestamp;
+}
+
+export interface UserObjectiveShares {
+  id: Generated<string>;
+  objectiveId: string;
+  userId: string;
+}
+
 export interface Users {
   email: string;
   id: Generated<string>;
-  name: string | null;
-  password: string | null;
+  login: string;
+  name: string;
+  password: string;
 }
 
 export interface DB {
+  objectives: Objectives;
+  "user-objective-shares": UserObjectiveShares;
   users: Users;
 }
