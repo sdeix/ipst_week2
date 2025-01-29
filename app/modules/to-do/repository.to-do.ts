3993a61f9fe4aaaa8144bd4408753eecca3d5646
sdeix <sdeix@q.com>
2025-01-29 11:53:12 +0700

    refactor gettodosbyquery repository 2

--- app/modules/to-do/repository.to-do.ts
+++ app/modules/to-do/repository.to-do.ts
@@ -1,4 +1,4 @@
-import { expressionBuilder, ExpressionWrapper, type Insertable, type Kysely, OperandExpression, RawBuilder, SqlBool, Transaction } from "kysely";
+import { type Insertable, type Kysely, OperandExpression, SqlBool, Transaction } from "kysely";
 import { DB, Objectives, UserObjectiveShares } from "../../common/types/kysely/db.type";
 import { GetToDoQueryType } from "./schemas/get-to-do.schema";
 import { UpdateToDoType } from "./schemas/update-to-do.schema";
@@ -22,29 +22,6 @@ export async function update(con: Kysely<DB> | Transaction<DB>, entity: UpdateTo
 }
 export async function getToDoById(con: Kysely<DB> | Transaction<DB>, id: string) {
     return await con.selectFrom("objectives").selectAll().where("id", "=", id).executeTakeFirst();
-}
-export async function var1GetToDosByQuery(con: Kysely<DB> | Transaction<DB>, query: GetToDoQueryType, userId: string) {
-    return await con
-        .selectFrom("objectives")
-        .selectAll()
-        .where((eb) =>
-            eb.or([
-                eb("creatorid", "=", userId),
-                eb.exists(
-                    con
-                        .selectFrom("user-objective-shares")
-                        .select("id")
-                        .where("user-objective-shares.userId", "=", userId)
-                        .whereRef("user-objective-shares.objectiveId", "=", "objectives.id" as any)
-                )
-            ])
-        )
-        .$if(query.search !== undefined, (qb) => qb.where("title", "ilike", `%${query.search}%`))
-        .$if(query.isCompleted !== undefined, (qb) => qb.where("isCompleted", "=", query.isCompleted === "true" ? true : false))
-        .orderBy(query.sortBy, query.sortOrder)
-        .limit(query.limit)
-        .offset(query.offset)
-        .execute();
 }
 export async function GetToDosByQuery(con: Kysely<DB> | Transaction<DB>, query: GetToDoQueryType, userId: string) {
     return await con
@@ -73,71 +50,6 @@ export async function GetToDosByQuery(con: Kysely<DB> | Transaction<DB>, query: 
         .offset(query.offset)
         .execute();
 }
-export async function var3GetToDosByQuery(con: Kysely<DB> | Transaction<DB>, query: GetToDoQueryType, userId: string) {
-    let conditions = con
-        .selectFrom("objectives")
-        .selectAll()
-        .where((eb) =>
-            eb.or([
-                eb("creatorid", "=", userId),
-                eb.exists(
-                    con
-                        .selectFrom("user-objective-shares")
-                        .select("id")
-                        .where("user-objective-shares.userId", "=", userId)
-                        .whereRef("user-objective-shares.objectiveId", "=", "objectives.id" as any)
-                )
-            ])
-        );
-
-    const filters: any[] = [];
-
-    if (query.search) {
-        filters.push((eb: any) => eb.where("title", "ilike", `%${query.search}%`));
-    }
-
-    if (query.isCompleted !== undefined) {
-        const isCompletedBool = query.isCompleted === "true";
-        filters.push((eb: any) => eb.where("isCompleted", "=", isCompletedBool));
-    }
-
-    filters.forEach((filter) => {
-        conditions = filter(conditions);
-    });
-
-    return await conditions.orderBy(query.sortBy, query.sortOrder).limit(query.limit).offset(query.offset).execute();
-}
-//  четвёртый вариант не мой, взял только для тестов скорости, после удалю
-export async function var4GetToDosByQuery(con: Kysely<DB> | Transaction<DB>, filters: GetToDoQueryType, userId: string) {
-    const query = con.selectFrom("objectives").where((eb) =>
-        eb.or([
-            eb("creatorid", "=", userId),
-            eb.exists(
-                con
-                    .selectFrom("user-objective-shares")
-                    .select("id")
-                    .where("user-objective-shares.userId", "=", userId)
-                    .whereRef("user-objective-shares.objectiveId", "=", "objectives.id" as any)
-            )
-        ])
-    );
-    const conditions: Array<ExpressionWrapper<DB, "objectives", SqlBool> | RawBuilder<any>> = [];
-
-    const eb = expressionBuilder<DB, "objectives">();
-    if (filters.isCompleted !== undefined) conditions.push(eb("objectives.isCompleted", "=", true));
-    if (filters.search !== undefined) conditions.push(eb("objectives.title", "ilike", `%${filters.search}%`));
-
-    const dataQuery = await query
-        .selectAll()
-        .where((eb) => eb.and(conditions))
-        .orderBy(filters.sortBy, filters.sortOrder)
-        .limit(filters.limit)
-        .offset(filters.offset)
-        .execute();
-
-    return dataQuery;
-}
-
 export async function share(con: Kysely<DB> | Transaction<DB>, entity: InsertableShareRowType) {
     return await con.insertInto("user-objective-shares").returningAll().values(entity).executeTakeFirstOrThrow();
 }
